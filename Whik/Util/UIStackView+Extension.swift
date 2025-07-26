6292e4b5bbcefcdd6c7be283f5084383ad2bd880
Eden <you8inpark@gmail.com>
2025-07-26 21:46:25 +0900

    feat: feat: build Home screen UI with swipeable destination cards

--- Whik/Util/UIStackView+Extension.swift
+++ Whik/Util/UIStackView+Extension.swift
@@ -8,7 +8,13 @@
 import UIKit
 
 extension UIStackView {
-    
+    func removeArrangedSubviews() {
+        arrangedSubviews.forEach {
+            removeArrangedSubview($0)
+            $0.removeFromSuperview()
+        }
+    }
+
     func addArrangedSubviews(_ views: UIView...) {
         views.forEach { addArrangedSubview($0) }
     }
